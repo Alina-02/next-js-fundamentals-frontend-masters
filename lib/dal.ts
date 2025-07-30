5d2c6f2f938e518d7a08bd35cc38956f523bec87
Alina-02 <alinamontma@gmail.com>
2025-07-30 21:07:49 +0200

    get user by email

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,4 +1,3 @@
-import { getUserByEmail } from '@/lib/dal';
 import { db } from '@/db';
 import { getSession } from './auth';
 import { eq } from 'drizzle-orm';
@@ -6,4 +5,31 @@ import { cache } from 'react';
 import { issues, users } from '@/db/schema';
 import { mockDelay } from './utils';
 
-export const getUserByEmail = () => {};
+export const getCurrentUser = () => {
+  const session = await getSession();
+  if (!session) return null;
+
+  try {
+    const result = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId));
+
+    return result[0] || null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getUserByEmail = (email: string) => {
+  try {
+    const user = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    });
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
